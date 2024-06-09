a58e756a9f3b80006f418c3b9d5588eb76ed406e
sungbumchoi <46594664+sungbumchoi@users.noreply.github.com>
2024-06-09 20:57:05 +0900

    섹션 6 - Users Microservice 2
     - Users Microservice - 인증과 권한 기능 개요
     - Users Microservice - AuthenticationFilter 추가
     - Users Microservice - loadUserByUsername() 구현

--- user-service/src/main/java/com/example/user_service/security/AuthenticationFilter.java
+++ user-service/src/main/java/com/example/user_service/security/AuthenticationFilter.java
@@ -1,10 +1,71 @@
 package com.example.user_service.security;
 
+import com.example.user_service.service.UserService;
+import com.example.user_service.vo.RequestLogin;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.sql.Array;
+import java.util.ArrayList;
 
 @Slf4j
-public class AuthenticationFilter {
-//
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private UserService userService;
+    private Environment environment;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager,
+                                   UserService userService, Environment environment) {
+        super(authenticationManager);
+        this.userService = userService;
+        this.environment = environment;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request
+                                              , HttpServletResponse response) throws AuthenticationException {
+        try {
+            // ObjectMapper().readValue()로 전달되어진 inputStream에 더떤 값이 들어가 있을 때 그 값을 
+            // 우리가 원하는 Java클래스 타입으로 변경
+            RequestLogin creds = new ObjectMapper().readValue(request.getInputStream(), RequestLogin.class);
+
+            // UsernamePasswordAuthenticationToken : 사용자가 입력한 아이디와 비밀번호 값을 Spring Security 에서
+            // 사용할 수 있는 형태의 값으로 변환
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            creds.getEmail(),
+                            creds.getPassword(),
+                             new ArrayList<>()
+                    )
+            );
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+
+
+    //
 //    private UserService userService;
 //    private Environment environment;
 //
--- user-service/src/main/java/com/example/user_service/security/WebSecurtiy.java
+++ user-service/src/main/java/com/example/user_service/security/WebSecurtiy.java
@@ -11,11 +11,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.IpAddressMatcher;
+
+import java.util.function.Supplier;
 
 //@Configuration
 //@EnableWebSecurity
@@ -37,13 +41,24 @@ public class WebSecurtiy {
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.csrf((csrf) -> csrf.disable());
+
+        http.csrf( (csrf) -> csrf.disable());
+//        http.csrf(AbstractHttpConfigurer::disable);
 
         http.authorizeHttpRequests((authz) -> authz
-                .requestMatchers(new AntPathRequestMatcher("/users/**")).permitAll()
+//                                .requestMatchers(new AntPathRequestMatcher("/actuator/**")).permitAll()
+//                                .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+//                                .requestMatchers(new AntPathRequestMatcher("/users", "POST")).permitAll()
+//                                .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+                        .requestMatchers("/**").access(this::hasIpAddress)
+                        .requestMatchers("/**").access(
+                                new WebExpressionAuthorizationManager("hasIpAddress('127.0.0.1') or hasIpAddress('172.30.1.48')"))
+                        .anyRequest().authenticated()
         );
 
         http.headers((headers) -> headers.frameOptions((frameOptions) -> frameOptions.sameOrigin()));
+
+        //-----------------------------------------------------------------
 
 //        // Configure AuthenticationManagerBuilder
 //        AuthenticationManagerBuilder authenticationManagerBuilder =
@@ -56,11 +71,11 @@ public class WebSecurtiy {
 ////        http.csrf(AbstractHttpConfigurer::disable);
 //
 //        http.authorizeHttpRequests((authz) -> authz
-//                                .requestMatchers(new AntPathRequestMatcher("/actuator/**")).permitAll()
-//                                .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
-//                                .requestMatchers(new AntPathRequestMatcher("/users", "POST")).permitAll()
-//                                .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
-////                        .requestMatchers("/**").access(this::hasIpAddress)
+////                                .requestMatchers(new AntPathRequestMatcher("/actuator/**")).permitAll()
+////                                .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+////                                .requestMatchers(new AntPathRequestMatcher("/users", "POST")).permitAll()
+////                                .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+//                        .requestMatchers("/**").access(this::hasIpAddress)
 //                                .requestMatchers("/**").access(
 //                                        new WebExpressionAuthorizationManager("hasIpAddress('127.0.0.1') or hasIpAddress('172.30.1.48')"))
 //                                .anyRequest().authenticated()
@@ -71,15 +86,15 @@ public class WebSecurtiy {
 //
 //        http.addFilter(getAuthenticationFilter(authenticationManager));
 //        http.headers((headers) -> headers.frameOptions((frameOptions) -> frameOptions.sameOrigin()));
-//
+////
         return http.build();
     }
 
-//    private AuthorizationDecision hasIpAddress(Supplier<Authentication> authentication, RequestAuthorizationContext object) {
-//        return new AuthorizationDecision(ALLOWED_IP_ADDRESS_MATCHER.matches(object.getRequest()));
-//    }
-//
-//    private AuthenticationFilterNew getAuthenticationFilter(AuthenticationManager authenticationManager) throws Exception {
-//        return new AuthenticationFilterNew(authenticationManager, userService, env);
-//    }
+    private AuthorizationDecision hasIpAddress(Supplier<Authentication> authentication, RequestAuthorizationContext object) {
+        return new AuthorizationDecision(ALLOWED_IP_ADDRESS_MATCHER.matches(object.getRequest()));
+    }
+
+    private AuthenticationFilter getAuthenticationFilter(AuthenticationManager authenticationManager) throws Exception {
+        return new AuthenticationFilter(authenticationManager, userService, env);
+    }
 }
